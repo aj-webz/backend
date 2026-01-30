f5a4353014a333747eead86988451b25277e15c9
Ajaay J <devox2049@gmail.com>
2026-01-30 19:34:26 +0530

    first commit

---
+++ src/index.ts
@@ -0,0 +1,43 @@
+import { Hono } from "hono";
+import { todos } from "./store/todo.store.js";
+
+const routes = new Hono();
+
+routes.get("/", (c) => {
+  return c.json(todos);
+});
+
+routes.post("/", async (c) => {
+  const data = await c.req.json();
+  todos.push(data);
+  return c.json(data, 201);
+});
+
+routes.patch("/:id/status", async (c) => {
+  const { id } = c.req.param();
+  const body = await c.req.json();
+
+  const todo = todos.find((t) => t.id === id);
+  if (!todo) {
+    return c.json({ message: "Todo not found" }, 404);
+  }
+
+  todo.status = body.status;
+  todo.completed = body.status === "completed";
+
+  return c.json(todo);
+});
+
+routes.delete("/:id", (c) => {
+  const { id } = c.req.param();
+
+  const index = todos.findIndex((t) => t.id === id);
+  if (index === -1) {
+    return c.json({ message: "Todo not found" }, 404);
+  }
+
+  todos.splice(index, 1);
+  return c.body(null, 204);
+});
+
+export default routes;
